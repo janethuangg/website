15234dfef4ea173d0a1ac118127c14a6a4241e18
janethuangg <jh6248@nyu.edu>
2020-08-13 13:48:04 -0400

    changed title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,31 @@ import Landing from "./Landing"
 import Container from "../components/Containers/Container"
 import About from "./About"
 import Project from "./Project"
+import { Helmet } from "react-helmet"
 
 const IndexPage = () => (
-  <Layout>
-    <Landing />
-    <Container>
-      <div id="about">
-        <About />
-      </div>
-      <div id="project">
-        <Project />
-      </div>
-    </Container>
-  </Layout>
+  <>
+    <Helmet>
+      <title>Janet Huang</title>
+      <link
+        rel="icon"
+        href={require("../images/logo.svg")}
+        type="image"
+        sizes="16x16"
+      />
+    </Helmet>
+    <Layout>
+      <Landing />
+      <Container>
+        <div id="about">
+          <About />
+        </div>
+        <div id="project">
+          <Project />
+        </div>
+      </Container>
+    </Layout>
+  </>
 )
 
 export default IndexPage
